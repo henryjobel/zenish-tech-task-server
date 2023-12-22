148d86fef1f9e706a0ef48e0fa9679976b56c4d5
henryjobel <jhobelsaha@gmail.com>
2023-12-22 09:58:13 +0600

    cors cerror solved

--- index.js
+++ index.js
@@ -1,58 +1,61 @@
 const express = require('express')
-const app = express()
-require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors')
-const cookieParser = require('cookie-parser')
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
-const morgan = require('morgan')
+require('dotenv').config();
+const app = express()
 const port = process.env.PORT || 5000
-// middleware
-const corsOptions = {
-    origin: ['https://task-management-server-zeta-indol.vercel.app', 'http://localhost:5173'],
-    credentials: true,
-    optionSuccessStatus: 200,
-}
-app.use(cors(corsOptions))
-app.use(express.json())
-app.use(cookieParser())
-app.use(morgan('dev'))
 
-const client = new MongoClient(process.env.DB_URI, {
+// middleware
+app.use(cors())
+app.use(express.json())
+
+// mongoDB
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cmmcrj9.mongodb.net/?retryWrites=true&w=majority`;
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
         strict: true,
         deprecationErrors: true,
-    },
-})
+    }
+});
 
-// code 
 async function run() {
     try {
-        const taskCollection = client.db('taskyDB').collection('task');
+
+        //change menuCollection with database name and collection.
+        const taskCollection = client.db('taskyDB').collection("task")
+
+        app.get('/task',async (req,res)=>{
+            const result = await taskCollection.find().toArray()
+            res.send(result)
+        })
 
         app.get('/task/:email', async (req, res) => {
-            const email = req.params.email;
-            const query = { email: email };
-            const result = await taskCollection.find(query).toArray();
-            res.send(result);
-        });
+            const email = req.params.email
+            const query = { email: email }
+            const result = await taskCollection.find(query).toArray()
+            res.send(result)
+        })
 
         app.post('/task', async (req, res) => {
             const task = req.body;
-            const result = await taskCollection.insertOne(task);
-            res.send(result);
-        });
+            const result = await taskCollection.insertOne(task)
+            res.send(result)
+        })
 
         app.delete('/task/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: new ObjectId(id) };
-            const result = await taskCollection.deleteOne(query);
-            res.send(result);
-        });
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await taskCollection.deleteOne(query)
+            res.send(result)
+        })
 
         app.put('/task/:id', async (req, res) => {
-            const id = req.params.id;
-            const filter = { _id: new ObjectId(id) };
+            const id = req.params.id
+            const filter = { _id: new ObjectId(id) }
             const updatedTask = req.body;
             const task = {
                 $set: {
@@ -61,23 +64,33 @@ async function run() {
                     Description: updatedTask.Description,
                     Deadline: updatedTask.Deadline,
                     status: updatedTask.status,
-                },
-            };
-            const result = await taskCollection.updateOne(filter, task);
-            res.send(result);
-        });
+                }
+            }
+            const result = await taskCollection.updateOne(filter, task)
+            res.send(result)
+
+        })
+
+
+
+
+        // Send a ping to confirm a successful connection
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
     }
 }
+run().catch(console.dir);
 
-run().catch(console.dir)
 
+
+// testing
 app.get('/', (req, res) => {
-    res.send(' evoLearn server is runung..')
+    res.send('Server is running')
 })
 
 app.listen(port, () => {
-    console.log(`EvoLearn is running on port ${port}`)
+    console.log(`Server is running on port: ${port}`);
 })
